a698c4fcd09acfd10ee6c15f9d29dda3209739ab
Farukov <vladislavfarukov2004@gmail.com>
2022-12-05 23:51:22 +0700

    Commit1

--- fa_learn_app/dependencies.py
+++ fa_learn_app/dependencies.py
@@ -2,6 +2,7 @@ from fa_learn_app.repositories.product import ProductTmpRepository
 
 TMP_REPOSITORY = ProductTmpRepository()
 
+
 def get_product_repo() -> ProductTmpRepository:
     # Получение Product репозитория
 
--- fa_learn_app/repositories/product.py
+++ fa_learn_app/repositories/product.py
@@ -1,20 +1,27 @@
 from typing import List, Dict, Optional
 import uuid
 from fa_learn_app.models.product import ProductIn, ProductOut, ProductStorage
-from fa_learn_app.utils.repository_utils import convert_product_storage_to_out, convert_product_in_to_storage
+from fa_learn_app.utils.repository_utils import convert_product_storage_to_out, convert_product_in_to_storage, update_product_in_storage
 
 
 class BaseProductRepository:
     # Базовый класс для реализации функционала работы с продуктами
 
-    def get_by_id(self, id :uuid.UUID | int) -> ProductOut:
+    def get_by_id(self, id: uuid.UUID) -> ProductOut:
         raise NotImplementedError
 
-    def get_all(self, limit :int, skip :int) -> List[ProductOut]:
+    def get_all(self, limit: int, skip: int) -> List[ProductOut]:
         raise NotImplementedError
 
-    def create(self, product :ProductIn) ->ProductOut:
+    def create(self, product: ProductIn) -> ProductOut:
         raise NotImplementedError
+
+    def update_by_id(self, id: uuid.UUID, product: ProductIn) -> ProductStorage:
+        raise NotImplementedError
+
+    def delete_by_id(self, id: uuid.UUID) -> ProductOut:
+        raise NotImplementedError
+
 
 class ProductTmpRepository(BaseProductRepository):
     # Реализация продукта с временным хранилищем в объекте Dict
@@ -22,24 +29,24 @@ class ProductTmpRepository(BaseProductRepository):
     def __init__(self):
 
         # Временное хранилище
-        self._dict_products :Dict[uuid.UUID, ProductStorage] = {}
+        self._dict_products: Dict[uuid.UUID, ProductStorage] = {}
 
-    def get_by_id(self, id :uuid.UUID) -> Optional[ProductOut]:
+    def get_by_id(self, id: uuid.UUID) -> Optional[ProductOut]:
         # Получение продукта по id
 
-        product :ProductStorage = self._dict_products.get(id)
+        product: ProductStorage = self._dict_products.get(id)
         if product is None:
-            return None
-        product_out :ProductOut = convert_product_storage_to_out(product)
+            return "Продукт не найден"
+        product_out: ProductOut = convert_product_storage_to_out(product)
         return product_out
 
-    def get_all(self, limit :int, skip :int) -> List[ProductOut]:
+    def get_all(self, limit: int, skip: int) -> List[ProductOut]:
         # Получение всех продуктов
 
-        product_out_list :List[ProductOut] = []
+        product_out_list: List[ProductOut] = []
         for _, product in self._dict_products.items():
             product_out_list.append(convert_product_storage_to_out(product))
-            return  product_out_list[skip:skip+limit]
+        return product_out_list[skip:skip + limit]
 
     def create(self, product: ProductIn) -> ProductOut:
         # Создание продукта
@@ -48,3 +55,22 @@ class ProductTmpRepository(BaseProductRepository):
         self._dict_products.update({product_storage.id: product_storage})
         product_out: ProductOut = convert_product_storage_to_out(product_storage)
         return product_out
+
+    def update_by_id(self, id: uuid.UUID, product_new: ProductIn) -> Optional[ProductStorage]:
+        # Получение продукта по id для обновления данных
+
+        product_old: ProductStorage = self._dict_products.get(id)
+        if product_old is None:
+            return "Продукт не найден"
+
+        product_out: ProductOut = update_product_in_storage(id, product_old.secret_token, product_new)
+        return product_out
+
+    def delete_by_id(self, id: uuid.UUID) -> str:
+        # Получение продукта по id
+
+        product: ProductStorage = self._dict_products.get(id)
+        if product is None:
+            return "Продукт не найден"
+        self._dict_products.pop(id, None)
+        return "Продукт успешно удален"
